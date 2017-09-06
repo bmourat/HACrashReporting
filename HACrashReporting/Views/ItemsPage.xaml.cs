9a6d3e7573fd8875050416464ba22ab5b98c848a
Murat Baysangurov <v-mubays@microsoft.com>
2017-09-06 14:39:20 +0300

    Initial commit

---
+++ HACrashReporting/Views/ItemsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HACrashReporting
+{
+    public partial class ItemsPage : ContentPage
+    {
+        public ItemsPage()
+        {
+            InitializeComponent();
+        }
+		void CrashMeClicked(object sender, EventArgs e)
+		{
+			int x = 0;
+			x = 4 / x;
+		}
+    }
+}
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using HockeyApp.iOS;
+
+namespace HACrashReporting.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+			var manager = BITHockeyManager.SharedHockeyManager;
+			manager.Configure("0a0e90bde4864fdfac65d7cc1b49da3f");
+            manager.LogLevel = BITLogLevel.Verbose;
+			manager.StartManager();
+			manager.Authenticator.AuthenticateInstallation();
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
